6831d09442da9705d4a58b1d4e54d60eb99deac7
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-03-20 16:11:10 +0530

    models for kurta and pyjama measurements

--- app/Models/KurtaMeasurementField.php
+++ app/Models/KurtaMeasurementField.php
@@ -15,5 +15,71 @@ class KurtaMeasurementField extends Model
             'field_en' => 'Length',
             'field_hi' => 'लम्बाई',
         ],
+        [
+            'id' => 2,
+            'field_en' => 'Shoulder',
+            'field_hi' => 'कन्धा',
+        ],
+        [
+            'id' => 3,
+            'field_en' => 'Chest',
+            'field_hi' => 'छाती',
+        ],
+        [
+            'id' => 4,
+            'field_en' => 'Stomach',
+            'field_hi' => 'पेट',
+        ],
+        [
+            'id' => 5,
+            'field_en' => 'Sleeves',
+            'field_hi' => 'बाहे',
+        ],
+        [
+            'id' => 6,
+            'field_en' => 'Neck',
+            'field_hi' => 'गला',
+        ],
+        [
+            'id' => 7,
+            'field_en' => 'Arm Hole',
+            'field_hi' => 'बुककया',
+        ],
+        [
+            'id' => 8,
+            'field_en' => 'Width',
+            'field_hi' => 'घेर',
+        ],
+        [
+            'id' => 9,
+            'field_en' => 'Sleeve Hole',
+            'field_hi' => 'मुरी',
+        ],
+        [
+            'id' => 10,
+            'field_en' => 'Hip',
+            'field_hi' => 'सीट',
+        ],
+        [
+            'id' => 11,
+            'field_en' => 'Breast',
+            'field_hi' => 'खिडक',
+        ]
+
     ];
 }
+/*
+1 LENGTH लम्बाई
+2 SHOULDER कन्धा
+3 CHEST छाती
+4 STOMACH पेट
+5 SLEEVES बाहे
+6 NECK गला
+7 ARM HOLE बुककया
+8 WIDTH घेर
+9 SLEEVE
+HOLE
+मुरी
+10 HIP सीट
+11 BREAST खिडक
+*/
--- app/Models/PyjamaMeasurementField.php
+++ app/Models/PyjamaMeasurementField.php
@@ -15,5 +15,57 @@ class PyjamaMeasurementField extends Model
             'field_en' => 'Length',
             'field_hi' => 'लम्बाई',
         ],
+        [
+            'id' => 2,
+            'field_en' => 'Hip',
+            'field_hi' => 'सीट',
+        ],
+        [
+            'id' => 3,
+            'field_en' => 'Waist',
+            'field_hi' => 'कमर',
+        ],
+        [
+            'id' => 4,
+            'field_en' => 'Bottom',
+            'field_hi' => 'बॉटम',
+        ],
+        [
+            'id' => 5,
+            'field_en' => 'Knees',
+            'field_hi' => 'घुटना',
+        ],
+        [
+            'id' => 6,
+            'field_en' => 'Calf',
+            'field_hi' => 'कपिंडी',
+        ],
+        [
+            'id' => 7,
+            'field_en' => 'Inside',
+            'field_hi' => 'इन्साइड',
+        ],
+        [
+            'id' => 8,
+            'field_en' => 'Thighs',
+            'field_hi' => 'जािंघ',
+        ],
+        [
+            'id' => 9,
+            'field_en' => 'Miyan',
+            'field_hi' => 'कमयानी',
+        ]
     ];
 }
+
+/*
+1 LENGTH लम्बाई
+2 HIP सीट कमर बॉटम घुटना कपिंडी इन्साइड
+3 WAIST कमर
+4 BOTTOM बॉटम
+5 KNEES घुटना
+6 CALF कपिंडी
+7 INSIDE इन्साइड
+8 THIGHS जािंघ िं
+9 MIYANI कमयानी
+*/
