a0743d8f4646e8dd045b16bd21c6e8e54254335b
pushpak1300 <pushpak1300@gmail.com>
2021-03-20 11:09:14 +0530

    Init

---
+++ resources/js/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { InertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+
+InertiaProgress.init({
+    color: '#ED8936',
+    showSpinner: true
+});
+
+const app = document.getElementById('app');
+
+render(
+    <InertiaApp
+        initialPage={JSON.parse(app.dataset.page)}
+        resolveComponent={name =>
+            import(`./Pages/${name}`).then(module => module.default)
+        }
+    />,
+    app
+);
